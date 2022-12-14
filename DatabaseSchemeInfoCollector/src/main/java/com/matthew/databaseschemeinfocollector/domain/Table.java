77ab7fa666d537e102f42c8d64700be2f5c21386
Mateusz Bednarczyk <3DJMatrix@38>
2022-12-14 21:50:42 +0100

    Scheme structure.

--- DatabaseSchemeInfoCollector/src/main/java/com/matthew/databaseschemeinfocollector/domain/Table.java
+++ DatabaseSchemeInfoCollector/src/main/java/com/matthew/databaseschemeinfocollector/domain/Table.java
@@ -1,14 +1,25 @@
 package com.matthew.databaseschemeinfocollector.domain;
 
+
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.HashSet;
+import java.util.Set;
 
 public class Table {
+    @NotBlank
     private String name;
-    private Column primaryKey;
-    private HashSet<Column> columns = new HashSet<>();
-    private HashSet<ForeignKey> foreignKeys = new HashSet<>();
 
-    public Table(String name, HashSet<Column> columns, HashSet<ForeignKey> foreignKeys) {
+    @NotBlank
+    private Column primaryKey;
+
+    @NotBlank
+    private Set<Column> columns = new HashSet<>();
+
+    @NotBlank
+    private Set<ForeignKey> foreignKeys = new HashSet<>();
+
+    public Table(String name, Set<Column> columns, Set<ForeignKey> foreignKeys) {
         this.name = name;
         this.primaryKey = new Column("ID");
         this.columns = columns;
